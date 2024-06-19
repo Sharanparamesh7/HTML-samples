db57446459c81b60dd7fc2bd9a7790e1e9fba3cf
Sharanparamesh7 <sharanparamesh860@gamil.com>
2024-06-19 16:24:44 +0530

    first js file

---
+++ dynamic.js
@@ -0,0 +1,27 @@
+
+ /*   let car={
+        carname:"honda",
+        carcolor:"black",
+        carprice:800000
+    }
+    for(let x in car){
+        document.write(car[x])
+    }*/
+  /* var x="SHARAN"
+   document.write(x);
+   var a=4
+   var b=3
+   document.write(a+b)*/
+
+/*var s="htnkjavascriptcss"
+document.write(s.length)
+var newstr=s.slice(2,8)
+document.write(newstr)*/
+var x='a'
+var y='b'
+var a=new Array();
+a[0]=99
+var b=["lion","tiger","dog"]
+document.write(a+" ")
+document.write(b[0])
+document.write(" "+x+" "+y)
